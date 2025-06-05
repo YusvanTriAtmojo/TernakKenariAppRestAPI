7969e645780c3bf063f7c43d423f121f8a49c05f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-05 23:01:50 +0700

    Menambahkan factory konversi decode dan encode JSON

--- lib/data/model/request/admin/admin_profile_request.dart
+++ lib/data/model/request/admin/admin_profile_request.dart
@@ -6,4 +6,16 @@ class AdminProfileRequestModel {
     AdminProfileRequestModel({
         this.name,
     });
+
+    factory AdminProfileRequestModel.fromJson(String str) => AdminProfileRequestModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory AdminProfileRequestModel.fromMap(Map<String, dynamic> json) => AdminProfileRequestModel(
+        name: json["name"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "name": name,
+    };
 }

efa69fb1d51cae8192d952feab78e4daa5587e6c
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-06 13:47:50 +0700

    Membuat repository profile admin dengan fungsi addProfile admin

---
+++ lib/data/repository/profile_admin_repository.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:nativewrappers/_internal/vm/lib/developer.dart';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:kenari_app/data/model/request/admin/admin_profile_request.dart';
+import 'package:kenari_app/data/model/response/admin_profile_response_mode.dart';
+import 'package:kenari_app/services/service_http_client.dart';
+
+class ProfileAdminRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  final securesStorage = FlutterSecureStorage();
+
+  ProfileAdminRepository(this._serviceHttpClient);
+
+  Future<Either<String, AdminProfileResponseModel>> addProfile(
+    AdminProfileRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        'admin/profile',
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201) {
+        final profileResponse = AdminProfileResponseModel.fromMap(jsonResponse);
+        log("Add Admin Profile successful: ${profileResponse.message}");
+        return Right(profileResponse);
+      } else {
+        log("Add Admin Profile failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Create Profile failed");
+      }
+    } catch (e) {
+      log("Error in adding profile: $e");
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
+}

1d1e59b4fe07147b05804e54188b70f2d8d345e9
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-06 14:39:06 +0700

    Membuat repository Anak dengan fungsi tambah anak

---
+++ lib/data/repository/anak_repository.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:dartz/dartz.dart';
+import 'package:kenari_app/data/model/request/admin/anak_request_model.dart';
+import 'package:kenari_app/data/model/response/get_all_anak_response.dart';
+import 'package:kenari_app/services/service_http_client.dart';
+
+class AnakRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  AnakRepository(this._serviceHttpClient);
+
+  Future<Either<String, GetAnakById>> addAnak(
+    AnakRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.postWithToken(
+        "admin/anak",
+        requestModel.toJson(),
+      );
+
+      if (response.statusCode == 201) {
+        final jsonResponse = json.decode(response.body);
+        final profileResponse = GetAnakById.fromJson(jsonResponse);
+        return Right(profileResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while adding profile: $e");
+    }
+  }
+}

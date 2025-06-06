24299c07ff056de54dbc5b6d02730907ea706bbe
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-06 07:46:30 +0700

    Menambahkan model AuthResponse dengan data user, konversi JSON

--- lib/data/model/response/auth_response_model.dart
+++ lib/data/model/response/auth_response_model.dart
@@ -1,5 +1,33 @@
 import 'dart:convert';
 
+class AuthResponseModel {
+    final String? message;
+    final int? statusCode;
+    final User? user;
+
+    AuthResponseModel({
+        this.message,
+        this.statusCode,
+        this.user,
+    });
+
+    factory AuthResponseModel.fromJson(String str) => AuthResponseModel.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory AuthResponseModel.fromMap(Map<String, dynamic> json) => AuthResponseModel(
+        message: json["message"],
+        statusCode: json["status_code"],
+        user: json["data"] == null ? null : User.fromMap(json["data"]),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "message": message,
+        "status_code": statusCode,
+        "data": user?.toMap(),
+    };
+}
+
 class User {
     final int? id;
     final String? name;
